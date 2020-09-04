588064423be15aeb7fd125da73546cda92ebcc63
Denis Kopeykin <denis.k@razortheory.com>
2020-09-04 13:34:48 +0300

    changed help text

--- apps/bot/handlers/commands.py
+++ apps/bot/handlers/commands.py
@@ -3,5 +3,19 @@ from .decorators import command_handler
 
 @command_handler("/start")
 def handle_start_command(message):
-    text = "Напиши username того с кем хочешь поиграть \(например `@my\_friend`\):"
-    message.bot.sendMessage(message.chat.id, text, parse_mode="MarkdownV2")
+    send_help_text(message.bot, message.chat.id)
+
+
+@command_handler("/help")
+def handle_start_command(message):
+    send_help_text(message.bot, message.chat.id)
+
+
+def send_help_text(bot, chat_id):
+    text = (
+        "Привет\! Здесь ты можешь поиграть в шахматы с реальными людьми\. "
+        "Для того чтобы начать партию напиши _`@username`_ пользователя с которым "
+        "ты желаешь играть\.\n\n*Важно\! Бот должен знать твоего оппонента, "
+        f"попроси его перейти к @dev\_chess\_bot и нажать /start*"
+    )
+    bot.sendMessage(chat_id, text, parse_mode="MarkdownV2")
