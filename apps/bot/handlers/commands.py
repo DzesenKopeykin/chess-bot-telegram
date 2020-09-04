14930d0da2e7520a6c269c9c4dd5cd314529a488
Denis Kopeykin <denis.k@razortheory.com>
2020-09-04 15:15:45 +0300

    create and update users when /start command was called

--- apps/bot/handlers/commands.py
+++ apps/bot/handlers/commands.py
@@ -1,9 +1,29 @@
 from .decorators import command_handler
+from ..models import User
 
 
 @command_handler("/start")
 def handle_start_command(message):
+    tele_user = message.from_user
+    User.update_or_create(tele_user)
     send_help_text(message.bot, message.chat.id)
+    if not tele_user.username:
+        text = (
+            "Я вижу что у вас не установлен `@username`\.\n"
+            "*Другие игроки не смогут предложить вам сыграть партию\!*\n"
+            "О том как установить `@username` и что это такое вы можете прочитать "
+            "[здесь](https://telegram.org/faq#q-what-are-usernames-how-do-i-get-one)\."
+            "\n\nПосле того как вы установили или изменили `@username` "
+            "запустите снова команду /start чтобы я запомнил его\."
+            "\n\nЕсли вы не хотите устанавливать себе `@username`, "
+            "то напишите `@username` соперника\."
+        )
+        message.bot.sendMessage(
+            message.chat.id,
+            text,
+            parse_mode="MarkdownV2",
+            disable_web_page_preview=True,
+        )
 
 
 @command_handler("/help")
@@ -13,9 +33,9 @@ def handle_start_command(message):
 
 def send_help_text(bot, chat_id):
     text = (
-        "Привет\! Здесь ты можешь поиграть в шахматы с реальными людьми\. "
-        "Для того чтобы начать партию напиши _`@username`_ пользователя с которым "
-        "ты желаешь играть\.\n\n*Важно\! Бот должен знать твоего оппонента, "
-        f"попроси его перейти к @dev\_chess\_bot и нажать /start*"
+        "Здравствуйте\! Здесь вы можете поиграть в шахматы с реальными людьми\. "
+        "Для того чтобы начать партию напишите `@username` пользователя с которым "
+        "вы желаете играть\.\n\n*Важно\! Бот должен знать вашего оппонента, "
+        f"попросите его перейти к @dev\_chess\_bot и нажать /start*"
     )
     bot.sendMessage(chat_id, text, parse_mode="MarkdownV2")
--- apps/bot/models.py
+++ apps/bot/models.py
@@ -7,3 +7,15 @@ class User(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True, null=True)
+
+    @classmethod
+    def update_or_create(cls, tele_user):
+        return cls.objects.update_or_create(
+            id=tele_user.id,
+            defaults=dict(
+                is_bot=tele_user.is_bot,
+                first_name=tele_user.first_name,
+                last_name = tele_user.last_name,
+                username=tele_user.username,
+            )
+        )
