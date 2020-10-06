76edd9b9a2dc67528e6c2f5e887a344bee41e99d
Denis Kopeykin <kopeykindzzesen@gmail.com>
2020-10-06 17:44:35 +0300

    added chess fields to user model

--- apps/bot/models.py
+++ apps/bot/models.py
@@ -2,11 +2,21 @@ from django.db import models
 
 
 class User(models.Model):
+    COLORS = (
+        ("w", "white"),
+        ("b", "black"),
+    )
+
     id = models.IntegerField(primary_key=True)
     is_bot = models.BooleanField(default=False)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True, null=True)
+
+    in_game = models.BooleanField(default=False)
+    board = models.CharField(max_length=255, null=True)
+    opponent = models.ForeignKey("self", null=True, on_delete=models.SET_NULL)
+    color = models.CharField(max_length=1, choices=COLORS, null=True)
 
     @classmethod
     def update_or_create(cls, tele_user):
@@ -15,7 +25,7 @@ class User(models.Model):
             defaults=dict(
                 is_bot=tele_user.is_bot,
                 first_name=tele_user.first_name,
-                last_name = tele_user.last_name or "",
+                last_name=tele_user.last_name or "",
                 username=tele_user.username,
-            )
+            ),
         )
