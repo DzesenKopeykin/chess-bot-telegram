ce0ba9c8cf35304d8a2cd499f6d097ccba96aab6
Denis Kopeykin <kopeykindzzesen@gmail.com>
2020-10-08 20:07:17 +0300

    use annotation for message handlers

--- apps/bot/handlers/messages.py
+++ apps/bot/handlers/messages.py
@@ -1,54 +1,51 @@
-from telegram import MessageEntity, Update
+from telegram import MessageEntity, Update, User, Message
 from telegram.ext import Filters, MessageHandler
 from telegram.ext.callbackcontext import CallbackContext
 
 from .. import dispatcher
-from ..models import User
+from ..models import User as DBUser
 
 
-def handle_username_message(update: Update, context: CallbackContext):
-    user, message, bot = update.effective_user, update.effective_message, context.bot
-    opponent_username = message.text[1:]  # remove @ symbol
+def handle_username_message(update: Update, context: CallbackContext) -> None:
+    player: User = update.effective_user
+    opponent_username = context.match.string[1:]  # remove @ symbol
 
-    if opponent_username == user.username:
+    if opponent_username == player.username:
         text = "Вы не можете начать партию с собой."
-        bot.sendMessage(user.id, text)
+        player.send_message(text)
         return
 
-    opponent = User.objects.filter(username=opponent_username).first()
+    opponent_db: DBUser = DBUser.objects.filter(username=opponent_username).first()
 
-    if not opponent:
+    if not opponent_db:
         text = (
             "ChessBot не знает этого пользователя."
             "Возможно он не открывал этого бота. "
             "Используйте команду /help_how_to_start чтобы узнать больше."
         )
-        bot.sendMessage(message.chat.id, text)
+        player.send_message(text)
 
     else:
-        user_mention = user.full_name
-        if user.username:
-            user_mention += f" (@{user.username})"
         text = (
-            f"Здравствуйте, {opponent.first_name}! {user_mention} "
-            "предлагает сыграть в шахматы. Вы согласны?"
+            f"Здравствуйте, {opponent_db.first_name}!\n"
+            f"{player.name} предлагает сыграть в шахматы. Вы согласны?"
         )
-        bot.sendMessage(opponent.id, text)
+        context.bot.sendMessage(opponent_db.id, text)
 
         text = (
-            f"Я отправил ваше предложение @{opponent.username}. "
+            f"Я отправил ваше предложение @{opponent_username}. "
             "Партия начнётся когда он(а) согласится."
         )
-        bot.sendMessage(user.id, text)
+        player.send_message(text)
 
 
-def handle_unknown_message(update: Update, context: CallbackContext):
+def handle_unknown_message(update: Update, context: CallbackContext) -> None:
     message = f"Моя твоя не понимать\.\nНапиши /help, чтобы узнать, что я понимаю\."
-    context.bot.sendMessage(update.effective_user.id, message, parse_mode="MarkdownV2")
+    update.effective_user.send_message(message, parse_mode="MarkdownV2")
 
 
-filters_for_username = Filters.text & Filters.entity(MessageEntity.MENTION)
-filters_for_any_message = Filters.all
+filter_for_username = Filters.regex("^@[a-zA-Z0-9_]{5,}$")
+dispatcher.add_handler(MessageHandler(filter_for_username, handle_username_message))
 
-dispatcher.add_handler(MessageHandler(filters_for_username, handle_username_message))
-dispatcher.add_handler(MessageHandler(filters_for_any_message, handle_unknown_message))
+filter_for_any_message = Filters.all
+dispatcher.add_handler(MessageHandler(filter_for_any_message, handle_unknown_message))
