155e1f700113d76fe3e797303b12d95321bd651f
Denis Kopeykin <kopeykindzzesen@gmail.com>
2020-10-19 18:45:43 +0300

    pass to callback data user id

--- apps/bot/builders/messages.py
+++ apps/bot/builders/messages.py
@@ -1,6 +1,7 @@
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, User
 
 from ..models import User as DBUser
+from ..utils import build_mention
 
 
 class BaseMessage:
@@ -22,18 +23,16 @@ class AskStartGameMessage(BaseMessage):
         super().__init__()
 
     def _build_text(self) -> str:
-        user_mention = self.player.full_name
-        if self.player.username:
-            user_mention += f" (@{self.player.username})"
-
         return (
             f"Здравствуйте, {self.opponent_db.first_name}!\n"
-            f"{user_mention} предлагает сыграть в шахматы. Вы согласны?"
+            f"{build_mention(self.player)} предлагает сыграть в шахматы. Вы согласны?"
         )
 
     def _build_keyboard(self) -> InlineKeyboardMarkup:
         yes_button = InlineKeyboardButton(
-            "Да \U0001f44c", callback_data="yes_start_game"
+            "Да \U0001f44c", callback_data=f"yes_start_game|{self.player.id}"
         )
-        no_button = InlineKeyboardButton("Нет", callback_data="no_start_game")
+        no_button = InlineKeyboardButton(
+            "Нет", callback_data=f"no_start_game|{self.player.id}"
+        )
         return InlineKeyboardMarkup([[yes_button, no_button]])
